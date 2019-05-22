61a12f46134012d7a6d1d436b8cfa2f136e56e47
Stanislav Seliverstov <s.seliverstov@gmail.com>
2019-05-22 15:52:49 +0300

    allure-jest+jasmine example (via #22)

---
+++ packages/allure-jest/test/default-runner/allure-report.ts
@@ -0,0 +1,13 @@
+import { JasmineAllureReporter } from "allure-jasmine";
+import { AllureRuntime, Status, TestResult } from "allure-js-commons";
+
+const reporter = new JasmineAllureReporter(
+  new AllureRuntime({
+    resultsDir: "./out/allure-results",
+    testMapper: (result: TestResult) => {
+      if (result.status == Status.SKIPPED) result.fullName = `(WAS SKIPPED) ${result.fullName}`;
+      return result;
+    }
+  })
+);
+jasmine.getEnv().addReporter(reporter);
---
+++ packages/allure-jest/test/default-runner/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>../../dist/test/default-runner/allure-report.js"],
+  "roots": [
+    "<rootDir>../../dist/test/default-runner/"
+  ]
+};
