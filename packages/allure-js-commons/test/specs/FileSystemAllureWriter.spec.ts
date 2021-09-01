e0e703ae61cace53d988bf79a19e7f960556c364
Dmitry Baev <baev@users.noreply.github.com>
2021-09-01 19:07:40 +0300

    fix node 12 tests

--- packages/allure-js-commons/test/specs/FileSystemAllureWriter.spec.ts
+++ packages/allure-js-commons/test/specs/FileSystemAllureWriter.spec.ts
@@ -1,5 +1,5 @@
 import { AllureConfig, AllureRuntime, ContentType } from "../../dist";
-import { mkdtemp } from "fs/promises";
+import { promises as fs } from "fs";
 import path from "path";
 import * as os from "os";
 import { readdirSync, writeFileSync } from "fs";
@@ -7,21 +7,21 @@ import { expect } from "chai";
 
 describe("FileSystemAllureWriter", async () => {
   it("should save attachment from path", async () => {
-    const tmp = await mkdtemp(path.join(os.tmpdir(), 'foo-'));
+    const tmp = await fs.mkdtemp(path.join(os.tmpdir(), "foo-"));
     const allureResults = path.join(tmp, "allure-results");
 
     const config: AllureConfig = {
-      resultsDir: allureResults
+      resultsDir: allureResults,
     };
 
     const runtime = new AllureRuntime(config);
 
     const from = path.join(tmp, "test-attachment.txt");
-    await writeFileSync(from, "test content")
+    await writeFileSync(from, "test content");
 
-    runtime.writeAttachmentFromPath(from, { contentType: ContentType.TEXT})
+    runtime.writeAttachmentFromPath(from, { contentType: ContentType.TEXT });
 
     const resultFiles = readdirSync(allureResults);
     expect(resultFiles).length(1);
-  })
-})
+  });
+});
