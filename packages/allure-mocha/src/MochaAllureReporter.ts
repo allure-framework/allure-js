e9a993856bcfdc06bbe03c1c0666e2d1e5a62c6e
sskorol <serhii.s.korol@gmail.com>
2022-05-12 23:51:00 +0300

    fix(mocha): handle missing `reporterOptions` edge case
    Fixed an issue when `reporterOptions` can be missing by adding an alternative empty object, which can be correctly destructured with a default value to `resultsDir`.
    
    Fixes #430

--- packages/allure-mocha/src/MochaAllureReporter.ts
+++ packages/allure-mocha/src/MochaAllureReporter.ts
@@ -11,10 +11,10 @@ export class MochaAllureReporter extends Mocha.reporters.Base {
   constructor(readonly runner: Mocha.Runner, readonly opts: Mocha.MochaOptions) {
     super(runner, opts);
 
-    const { resultsDir } = opts.reporterOptions;
+    const { resultsDir = "allure-results" } = opts.reporterOptions || {};
     const allureConfig: AllureConfig = {
-      resultsDir: resultsDir || "allure-results",
       ...opts.reporterOptions,
+      resultsDir,
     };
 
     this.coreReporter = new AllureReporter(new AllureRuntime(allureConfig));
