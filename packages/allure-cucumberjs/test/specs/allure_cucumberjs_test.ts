f3c48413d782eefcd9a259262929e517abe75738
Konstantin Epishev <konstantin@epishev.me>
2022-06-27 15:14:52 +0200

    provide specs for host label setting

--- packages/allure-cucumberjs/test/specs/allure_cucumberjs_test.ts
+++ packages/allure-cucumberjs/test/specs/allure_cucumberjs_test.ts
@@ -1,3 +1,5 @@
+import sinon from "sinon";
+import os from "os";
 import { LabelName, Status } from "allure-js-commons";
 import { expect } from "chai";
 import { ITestFormatterOptions, runFeatures } from "../helpers/formatter_helpers";
@@ -354,6 +356,8 @@ describe("CucumberJSAllureReporter", () => {
   });
 
   it("should create labels", async () => {
+    sinon.stub(os, "hostname").returns("127.0.0.1");
+
     const results = await runFeatures(dataSet.withTags);
     expect(results.tests).length(1);
 
@@ -361,12 +365,14 @@ describe("CucumberJSAllureReporter", () => {
     const framework = results.tests[0].labels.find((label) => label.name === LabelName.FRAMEWORK);
     const feature = results.tests[0].labels.find((label) => label.name === LabelName.FEATURE);
     const suite = results.tests[0].labels.find((label) => label.name === LabelName.SUITE);
+    const host = results.tests[0].labels.find((label) => label.name === LabelName.HOST);
     const tags = results.tests[0].labels.filter((label) => label.name === LabelName.TAG);
 
     expect(language?.value).eq("javascript");
     expect(framework?.value).eq("cucumberjs");
     expect(feature?.value).eq("a");
     expect(suite?.value).eq("b");
+    expect(host?.value).eq("127.0.0.1");
     expect(tags).length(2);
     expect(tags[0].value).eq("@foo");
     expect(tags[1].value).eq("@bar");
