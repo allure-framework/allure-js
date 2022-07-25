0785277d7dc80e0801879e99f52ea35f781bd8ed
Steven Carter <stevencarter@crateandbarrel.com>
2022-07-25 07:07:30 -0500

    changed runner back to async only

--- packages/allure-cucumberjs/test/runner.ts
+++ packages/allure-cucumberjs/test/runner.ts
@@ -8,7 +8,7 @@ import "source-map-support/register";
 const mocha = new Mocha({
   timeout: 16000,
   reporter: "mocha-multi-reporters",
-  asyncOnly: false,
+  asyncOnly: true,
   reporterOptions: {
     reporterEnabled: "list, ../allure-mocha",
     allureMochaReporterOptions: {
