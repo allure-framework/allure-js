a0cb915ba0f13dbf585197717385ee18eb383f83
Vladimir Semenov <vovsemenv@gmail.com>
2022-12-09 15:33:13 +0400

    add-karma

---
+++ packages/allure-karma/README.md
@@ -0,0 +1,17 @@
+# allure-karma
+
+This project implements Allure integration with Karma
+
+## Installation
+
+```
+npm i karma-allure-reporter -D
+```
+
+## Usage
+
+Add **allure** into **karma.conf.js**:
+
+```
+    reporters: ["progress", "allure"],
+```
---
+++ packages/allure-karma/src/index.ts
@@ -0,0 +1,49 @@
+import path from "path";
+import { AllureRuntime, Status } from "allure-js-commons";
+
+interface KarmaResult {
+  fullName: string;
+  skipped: boolean;
+  success: boolean;
+  time: number;
+  log?: string[];
+}
+
+class AllureReporter {
+  static $inject: string[];
+
+  allureRuntime: AllureRuntime;
+  constructor() {
+    this.allureRuntime = new AllureRuntime({
+      resultsDir: defaultReportFolder(),
+    });
+  }
+
+  onSpecComplete(_browsers: any, result: KarmaResult) {
+    const group = this.allureRuntime.startGroup(result.fullName);
+    const test = group.startTest(result.fullName, 0);
+
+    if (result.skipped) {
+      test.status = Status.SKIPPED;
+    } else if (result.success) {
+      test.status = Status.PASSED;
+    } else {
+      test.status = Status.FAILED;
+    }
+
+    if (result.log) {
+      test.statusDetails = { message: result.log.toString() };
+    }
+    group.endGroup();
+  }
+}
+
+const defaultReportFolder = (): string => {
+  return path.resolve(process.cwd(), "allure-results");
+};
+
+AllureReporter.$inject = ["config"];
+
+export = {
+  "reporter:allure": ["type", AllureReporter],
+};
