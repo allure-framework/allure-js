9cd45579f5821b3dd2f8db94985cc2a9b692483f
Vladimir Semenov <vovsemenv@gmail.com>
2022-12-27 13:26:28 +0400

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,6 @@ module.exports = {
     "eslint-plugin-import",
     "eslint-plugin-jsdoc",
     "@typescript-eslint",
-    "eslint-plugin-prettier",
   ],
   rules: {
     "@typescript-eslint/adjacent-overload-signatures": "error",
