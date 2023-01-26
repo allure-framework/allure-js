7214b5e1ea280e3c43a9b96455414656f3f1fd5c
Vladimir Semenov <vovsemenv@gmail.com>
2023-01-27 00:46:28 +0300

    add tests

---
+++ packages/allure-jasmine/test/simple-fixtures.test.ts
@@ -0,0 +1,13 @@
+beforeAll(() => {});
+beforeEach(() => {});
+afterAll(() => {});
+afterEach(() => {});
+
+describe("example describe for fixtures", () => {
+  it("should pass no errors", () => {
+    expect(true).toBeTruthy();
+  });
+  it("should pass no errors", () => {
+    expect(false).toBeFalse();
+  });
+});
