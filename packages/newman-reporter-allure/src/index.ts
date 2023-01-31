2e11deae3a367eb092a4eced1aed7ed7ac61e250
Vladimir Semenov <vovsemenv@gmail.com>
2023-01-31 17:07:46 +0300

    fix-duration

--- packages/newman-reporter-allure/src/index.ts
+++ packages/newman-reporter-allure/src/index.ts
@@ -472,11 +472,11 @@ class AllureReporter {
 
       currStep.status = Status.FAILED;
       currStep.stage = Stage.FINISHED;
-      currStep.endStep();
     } else {
       currStep.stage = Stage.FINISHED;
       currStep.status = Status.PASSED;
     }
+    currStep.endStep();
   }
 
   onDone(_err: any, _args: unknown) {
