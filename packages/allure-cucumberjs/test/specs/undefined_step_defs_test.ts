30b69767d07e3c4ccdb46ee224194ea17e0c6c5c
epszaw <konstantin@epishev.me>
2023-04-14 15:35:41 +0200

    add spec for undefined steps

--- packages/allure-cucumberjs/test/specs/undefined_step_defs_test.ts
+++ packages/allure-cucumberjs/test/specs/undefined_step_defs_test.ts
@@ -6,13 +6,25 @@ import { ITestFormatterOptions, runFeatures } from "../helpers/formatter_helpers
 import { buildSupportCodeLibrary } from "../helpers/runtime_helpers";
 
 const dataSet: { [name: string]: ITestFormatterOptions } = {
-  withoutDefinedStepDefs: {
+  withoutTestImplementation: {
     supportCodeLibrary: buildSupportCodeLibrary(({ Given, When, Then }) => {
       Given("a step", function () {});
     }),
     sources: [
       {
         data: "Feature: a\n" + "\n" + "  Scenario: b\n" + "    Given an another step\n",
+        uri: "withUndefinedTest.feature",
+      },
+    ],
+  },
+  withoutStepImplementation: {
+    supportCodeLibrary: buildSupportCodeLibrary(({ Given, When, Then }) => {
+      Given("a step", function () {});
+      Then("result actually is", function () {});
+    }),
+    sources: [
+      {
+        data: "Feature: a\n\n" + "  Scenario: b\n" + "    Given a step\n" + "    Then result is\n",
         uri: "withUndefinedStep.feature",
       },
     ],
@@ -20,10 +32,20 @@ const dataSet: { [name: string]: ITestFormatterOptions } = {
 };
 
 describe("CucumberJSAllureReporter > with anonymous steps", () => {
-  it("provides details message that test doesn't have step definition", async () => {
-    const results = await runFeatures(dataSet.withoutDefinedStepDefs);
+  it("provides details message that test doesn't have implementation", async () => {
+    const results = await runFeatures(dataSet.withoutTestImplementation);
     expect(results.tests).length(1);
     expect(results.tests[0].status).eq(undefined);
     expect(results.tests[0].statusDetails.message).eq("The test doesn't have an implementation.");
+  });
+
+  it("provides details message that step doesn't have implementation", async () => {
+    const results = await runFeatures(dataSet.withoutStepImplementation);
+    expect(results.tests[0].steps).length(2);
+    expect(results.tests[0].steps[0].status).eq(Status.PASSED);
+    expect(results.tests[0].steps[1].status).eq(undefined);
+    expect(results.tests[0].steps[1].statusDetails.message).eq(
+      "The step doesn't have an implementation.",
+    );
   });
 });
