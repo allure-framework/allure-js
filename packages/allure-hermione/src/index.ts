7e70a60371a9d41b77de1a025011f51e1dd674d8
Konstantin Epishev <konstantin@epishev.me>
2023-05-10 20:16:28 +0200

    add correct test id generation for hermione reporter (fixes #655, fixes #656, via #675)

--- packages/allure-hermione/src/index.ts
+++ packages/allure-hermione/src/index.ts
@@ -45,8 +45,16 @@ const hermioneAllureReporter = (hermione: Hermione, opts: AllureReportOptions) =
     resultsDir: "allure-results",
     ...opts,
   });
+  const getTestId = (context: string | Hermione.Test) => {
+    if (typeof context === "string") {
+      return (testId?: string) => `${context}:${testId || ""}`;
+    }
+
+    return () => `${context.sessionId}:${context.id()}`;
+  };
   const handleTestError = (test: Hermione.Test, error: Hermione.TestError) => {
-    const currentTest = runningTests.get(test.id())!;
+    const testId = getTestId(test);
+    const currentTest = runningTests.get(testId())!;
     const { message, stack, screenshot } = error;
 
     currentTest.detailsMessage = message;
@@ -139,63 +147,65 @@ const hermioneAllureReporter = (hermione: Hermione, opts: AllureReportOptions) =
   };
 
   hermione.on(hermione.events.NEW_BROWSER, (browser) => {
-    browser.addCommand("label", async (testId: string, name: string, value: string) => {
-      await addLabel(testId, name, value);
+    const testId = getTestId(browser.sessionId);
+
+    browser.addCommand("label", async (id: string, name: string, value: string) => {
+      await addLabel(testId(id), name, value);
     });
-    browser.addCommand(
-      "link",
-      async (testId: string, url: string, name?: string, type?: string) => {
-        await addLink(testId, url, name, type);
-      },
-    );
+    browser.addCommand("link", async (id: string, url: string, name?: string, type?: string) => {
+      await addLink(testId(id), url, name, type);
+    });
     browser.addCommand(
       "parameter",
-      async (testId: string, name: string, value: string, options?: ParameterOptions) => {
-        await addParameter(testId, name, value, options);
+      async (id: string, name: string, value: string, options?: ParameterOptions) => {
+        await addParameter(testId(id), name, value, options);
       },
     );
-    browser.addCommand("id", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.ALLURE_ID, value);
+    browser.addCommand("id", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.ALLURE_ID, value);
     });
-    browser.addCommand("epic", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.EPIC, value);
+    browser.addCommand("epic", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.EPIC, value);
     });
-    browser.addCommand("feature", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.FEATURE, value);
+    browser.addCommand("feature", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.FEATURE, value);
     });
-    browser.addCommand("story", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.STORY, value);
+    browser.addCommand("story", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.STORY, value);
     });
-    browser.addCommand("suite", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.SUITE, value);
+    browser.addCommand("suite", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.SUITE, value);
     });
-    browser.addCommand("parentSuite", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.PARENT_SUITE, value);
+    browser.addCommand("parentSuite", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.PARENT_SUITE, value);
     });
-    browser.addCommand("subSuite", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.SUB_SUITE, value);
+    browser.addCommand("subSuite", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.SUB_SUITE, value);
     });
-    browser.addCommand("owner", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.OWNER, value);
+    browser.addCommand("owner", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.OWNER, value);
     });
-    browser.addCommand("severity", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.SEVERITY, value);
+    browser.addCommand("severity", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.SEVERITY, value);
     });
-    browser.addCommand("tag", async (testId: string, value: string) => {
-      await addLabel(testId, LabelName.TAG, value);
+    browser.addCommand("tag", async (id: string, value: string) => {
+      await addLabel(testId(id), LabelName.TAG, value);
     });
-    browser.addCommand("issue", async (testId: string, name: string, url: string) => {
-      await addLink(testId, url, name, LinkType.ISSUE);
+    browser.addCommand("issue", async (id: string, name: string, url: string) => {
+      await addLink(testId(id), url, name, LinkType.ISSUE);
     });
-    browser.addCommand("tms", async (testId: string, name: string, url: string) => {
-      await addLink(testId, url, name, LinkType.TMS);
+    browser.addCommand("tms", async (id: string, name: string, url: string) => {
+      await addLink(testId(id), url, name, LinkType.TMS);
     });
-    browser.addCommand("attach", async (testId: string, source: string, mimetype: string) => {
-      await addAttachment(testId, source, mimetype);
+    browser.addCommand("attach", async (id: string, source: string, mimetype: string) => {
+      await addAttachment(testId(id), source, mimetype);
     });
-    browser.addCommand("step", async (testId: string, name: string, body: StepBodyFunction) => {
+    browser.addCommand("step", async (id: string, name: string, body: StepBodyFunction) => {
       const step = new AllureCommandStepExecutable(name);
-      await step.run(body, async (message: MetadataMessage) => await sendMetadata(testId, message));
+      await step.run(
+        body,
+        async (message: MetadataMessage) => await sendMetadata(testId(id), message),
+      );
     });
   });
   hermione.on(hermione.events.NEW_WORKER_PROCESS, (worker) => {
@@ -210,6 +220,7 @@ const hermioneAllureReporter = (hermione: Hermione, opts: AllureReportOptions) =
     });
   });
   hermione.on(hermione.events.TEST_BEGIN, (test) => {
+    const testId = getTestId(test);
     const { ALLURE_HOST_NAME, ALLURE_THREAD_NAME } = process.env;
     const thread = ALLURE_THREAD_NAME || test.sessionId;
     const hostnameLabel = ALLURE_HOST_NAME || hostname;
@@ -238,15 +249,17 @@ const hermioneAllureReporter = (hermione: Hermione, opts: AllureReportOptions) =
       currentTest.addLabel(LabelName.SUB_SUITE, subSuites.join(" > "));
     }
 
-    runningTests.set(test.id(), currentTest);
+    runningTests.set(testId(), currentTest);
   });
   hermione.on(hermione.events.TEST_PASS, (test) => {
-    const currentTest = runningTests.get(test.id())!;
+    const testId = getTestId(test);
+    const currentTest = runningTests.get(testId())!;
 
     currentTest.status = Status.PASSED;
   });
   hermione.on(hermione.events.TEST_FAIL, (test) => {
-    const currentTest = runningTests.get(test.id());
+    const testId = getTestId(test);
+    const currentTest = runningTests.get(testId());
 
     // hermione handle all errors in this hook, even test hasn't been started
     if (!currentTest) {
@@ -256,7 +269,8 @@ const hermioneAllureReporter = (hermione: Hermione, opts: AllureReportOptions) =
     currentTest.status = Status.FAILED;
   });
   hermione.on(hermione.events.TEST_END, (test) => {
-    const currentTest = runningTests.get(test.id())!;
+    const testId = getTestId(test);
+    const currentTest = runningTests.get(testId())!;
 
     if (test.err) {
       handleTestError(test, test.err);
@@ -264,7 +278,7 @@ const hermioneAllureReporter = (hermione: Hermione, opts: AllureReportOptions) =
 
     currentTest.stage = Stage.FINISHED;
     currentTest.endTest(Date.now());
-    runningTests.delete(test.id());
+    runningTests.delete(testId());
   });
 
   // it needs for tests because we need to read runtime writer data redefined in hermione config
