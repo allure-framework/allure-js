64e1b27524249b58292da044255b2d4a768627ad
epszaw <konstantin@epishev.me>
2023-06-07 14:45:09 +0200

    add test for hermione browser commands inside hooks

---
+++ packages/allure-hermione/test/spec/hooks.test.ts
@@ -0,0 +1,27 @@
+import { TestResult } from "allure-js-commons";
+import { expect } from "chai";
+import Hermione from "hermione";
+import { beforeEach, describe, it } from "mocha";
+import { HermioneAllure } from "../types";
+
+describe("hooks", () => {
+  let hermione: HermioneAllure;
+
+  beforeEach(async () => {
+    hermione = new Hermione("./test/.hermione.conf.js") as HermioneAllure;
+
+    await hermione.run(["./test/fixtures/hooks.js"], {});
+  });
+
+  it("applies commands from `beforeEach` and `afterEach` for each test", () => {
+    const hasHookLabel = (result: TestResult, hook: string) => {
+      return !!result.labels.find(({ name, value }) => name === "hook" && value === hook);
+    };
+
+    expect(
+      hermione.allure.writer.results.every(
+        (result) => hasHookLabel(result, "before") && hasHookLabel(result, "after"),
+      ),
+    ).eq(true);
+  });
+});
