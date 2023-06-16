ec73e09ce15a348760f1a2c3d08c715d0aefa5f6
epszaw <konstantin@epishev.me>
2023-06-16 10:22:32 +0200

    return back hermione attachment check using sinon

--- packages/allure-hermione/test/spec/attachments.test.ts
+++ packages/allure-hermione/test/spec/attachments.test.ts
@@ -1,11 +1,20 @@
 import * as AllureCommons from "allure-js-commons";
 import { expect } from "chai";
-import { before, describe, it } from "mocha";
+import Sinon from "sinon";
+import { beforeEach, describe, it } from "mocha";
 import { getTestResultByName } from "../runner";
 import { HermioneAllure } from "../types";
 import Hermione from "hermione";
 
 describe("attachments", () => {
+  let writeAttachmentStub: Sinon.SinonStub;
+
+  beforeEach(() => {
+    Sinon.restore();
+
+    writeAttachmentStub = Sinon.stub(AllureCommons.AllureRuntime.prototype, "writeAttachment");
+  });
+
   it("adds json attachment", async () => {
     const hermione = new Hermione("./test/.hermione.conf.js") as HermioneAllure;
 
@@ -18,5 +27,10 @@ describe("attachments", () => {
 
     expect(attachment.name).eq("Attachment");
     expect(attachment.type).eq("application/json");
+    expect(writeAttachmentStub.firstCall.args).eql([
+      JSON.stringify({ foo: "bar" }),
+      "application/json",
+      "utf8",
+    ]);
   });
 });
