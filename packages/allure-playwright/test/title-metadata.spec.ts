8c7477c2cd62e6625955361d9eee9251af202430
Vladimir Semenov <vovsemenv@gmail.com>
2023-08-09 11:25:56 +0300

    add more tests

--- packages/allure-playwright/test/title-metadata.spec.ts
+++ packages/allure-playwright/test/title-metadata.spec.ts
@@ -22,3 +22,30 @@ test("should have metadata from title", async ({ runInlineTest }) => {
 
   expect(results.tests[0].fullName).toBe("a.test.ts#some strange name to test");
 });
+
+test("multiline name", async ({ runInlineTest }) => {
+  const results = await runInlineTest({
+    "a.test.ts": /* ts */ `
+      import { test } from '@playwright/test';
+      import { allure } from '../../dist/index'
+      test(
+        \`some strange name to test @allure.label.l1=v1
+something else in name @allure.label.l2=v2 @allure.label.l3=v3 some word\`, async ({}, testInfo) => {
+      });
+      `,
+  });
+
+  expect(results.tests).toEqual([
+    expect.objectContaining({
+      labels: expect.arrayContaining([
+        { name: "l1", value: "v1" },
+        { name: "l2", value: "v2" },
+        { name: "l3", value: "v3" },
+      ]),
+    }),
+  ]);
+
+  expect(results.tests[0].fullName).toBe(
+    "a.test.ts#some strange name to test \nsomething else in name   some word",
+  );
+});
