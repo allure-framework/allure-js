3f70a79443881928834089f653270af59e646ff2
Caio Gevegir <56521026+caiogevegir@users.noreply.github.com>
2023-08-17 11:41:31 -0300

    Update with_labels_test.ts with separated test cases

--- packages/allure-cucumberjs/test/specs/with_labels_test.ts
+++ packages/allure-cucumberjs/test/specs/with_labels_test.ts
@@ -19,12 +19,25 @@ const dataSet: { [name: string]: ITestFormatterOptions } = {
           "  Scenario: b\n" +
           "    Given a step\n" +
           "    When do something\n" +
-          "    Then get something\n" +
+          "    Then get something\n",
+        uri: "withIssueLink.feature",
+      },
+    ],
+  },
+  withLabelsAndRules: {
+    supportCodeLibrary: buildSupportCodeLibrary(({ Given }) => {
+      Given("a step", () => {});
+    }),
+    sources: [
+      {
+        data:
+          "@severity:foo @feature:bar\n" +
+          "Feature: a\n" +
           "\n" +
-          "  Rule: c\n" +
+          "  Rule: r\n" +
           "\n" +
-          "    @severity:qux @feature:bar @bar\n" +
-          "    Scenario: d\n" +
+          "    @severity:bar @feature:foo @foo\n" +
+          "    Scenario: b\n" +
           "      Given a step\n" +
           "      When do something\n" +
           "      Then get something\n",
@@ -59,12 +72,41 @@ describe("CucumberJSAllureReporter > examples", () => {
       .filter((label) => label.name === LabelName.FEATURE)
       .map(({ value }) => value);
 
-    expect(tags).length(2);
+    expect(tags).length(1);
     expect(severityLabels).contains("foo");
     expect(severityLabels).contains("bar");
-    expect(severityLabels).contains("qux");
     expect(featureLabels).contains("foo");
     expect(featureLabels).contains("bar");
-    expect(featureLabels).contains("qux");
+  });
+
+  it("should add labels when scenario is inside a rule", async () => {
+    const results = await runFeatures(dataSet.withLabelsAndRules, {
+      labels: [
+        {
+          pattern: [/@feature:(.*)/],
+          name: "feature",
+        },
+        {
+          pattern: [/@severity:(.*)/],
+          name: "severity",
+        },
+      ],
+    });
+    expect(results.tests).length(1);
+
+    const { labels } = results.tests[0];
+    const tags = labels.filter((label) => label.name === LabelName.TAG);
+    const severityLabels = labels
+      .filter((label) => label.name === LabelName.SEVERITY)
+      .map(({ value }) => value);
+    const featureLabels = labels
+      .filter((label) => label.name === LabelName.FEATURE)
+      .map(({ value }) => value);
+
+    expect(tags).length(1);
+    expect(severityLabels).contains("foo");
+    expect(severityLabels).contains("bar");
+    expect(featureLabels).contains("foo");
+    expect(featureLabels).contains("bar");
   });
 });
