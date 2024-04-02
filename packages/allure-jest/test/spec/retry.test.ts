2bc9f54616c64d793a4826b8f602508ccdafd379
Gosha Krikun <g.krikun@innopolis.ru>
2024-04-02 20:36:47 +0300

    test(allure-jest): add jest.retryTimes spec

---
+++ packages/allure-jest/test/spec/retry.test.ts
@@ -0,0 +1,28 @@
+import { expect, it } from "@jest/globals";
+import { runJestInlineTest } from "../utils";
+
+it("should work for test with retries", async () => {
+  const { tests, processError } = await runJestInlineTest(`
+    jest.retryTimes(1);
+
+    it("failure", async () => {
+      await allure.label("foo", "bar");
+      expect(false).toBeTruthy();
+    });
+  `);
+
+  expect(processError).toContain("FAIL");
+  expect(tests).toHaveLength(2);
+  expect(tests[0].labels).toContainEqual(
+    expect.objectContaining({
+      name: "foo",
+      value: "bar",
+    }),
+  );
+  expect(tests[1].labels).toContainEqual(
+    expect.objectContaining({
+      name: "foo",
+      value: "bar",
+    }),
+  );
+});
