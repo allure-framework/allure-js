c7402874f4873b3a8d79f8bc82b9349e6497af54
epszaw <konstantin@epishev.me>
2024-05-16 14:58:11 +0200

    fix eslint and prettier issues

--- packages/allure-cypress/test/utils.ts
+++ packages/allure-cypress/test/utils.ts
@@ -2,7 +2,7 @@ import { fork } from "node:child_process";
 import { randomUUID } from "node:crypto";
 import { mkdir, readFile, readdir, rm, writeFile } from "node:fs/promises";
 import { join, resolve as resolvePath } from "node:path";
-import type { AllureResults, TestResultContainer, TestResult } from "allure-js-commons";
+import type { AllureResults, TestResult, TestResultContainer } from "allure-js-commons";
 
 export const runCypressInlineTest = async (
   test: (allureCommonsModulePath: string) => string,
