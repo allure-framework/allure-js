68864bed15aa32c9dd1b75ec03303ef2090108b0
Dmitry Baev <baev@users.noreply.github.com>
2024-06-03 14:49:42 +0100

    fix facade types

--- packages/allure-js-commons/src/facade.ts
+++ packages/allure-js-commons/src/facade.ts
@@ -4,7 +4,12 @@ import { getGlobalTestRuntimeWithAutoconfig } from "./sdk/runtime/runtime.js";
 import type { TestRuntime } from "./sdk/runtime/types.js";
 import { isPromise } from "./sdk/utils.js";
 
-const callRuntimeMethod = (method: string, ...args: any[]) => {
+// eslint-disable-next-line @typescript-eslint/ban-types
+type ArgumentTypes<F extends Function> = F extends (...args: infer A) => any ? A : never;
+const callRuntimeMethod = <T extends keyof TestRuntime, S extends ArgumentTypes<TestRuntime[T]>>(
+  method: T,
+  ...args: S
+) => {
   const runtime = getGlobalTestRuntimeWithAutoconfig();
 
   if (!isPromise(runtime)) {
@@ -20,82 +25,45 @@ const callRuntimeMethod = (method: string, ...args: any[]) => {
 
 export const label = (name: LabelName | string, value: string) => {
   return callRuntimeMethod("labels", { name, value });
-  // const runtime = getGlobalTestRuntimeWithAutoconfig();
-  //
-  // if (!isPromise(runtime)) {
-  //   return (runtime as TestRuntime).labels({ name, value });
-  // }
-  //
-  // return (runtime as Promise<TestRuntime>).then((testRuntime) => testRuntime.labels({ name, value }));
 };
 
 export const labels = (...labelsList: Label[]) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.labels(...labelsList);
   return callRuntimeMethod("labels", ...labelsList);
 };
 
 export const link = (url: string, name?: string, type?: LinkType | string) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.links({ url, type, name });
   return callRuntimeMethod("links", { url, type, name });
 };
 
 export const links = (...linksList: Link[]) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.links(...linksList);
   return callRuntimeMethod("links", ...linksList);
 };
 
 export const parameter = (name: string, value: string, options?: ParameterOptions) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.parameter(name, value, options);
   return callRuntimeMethod("parameter", name, value, options);
 };
 
 export const description = (markdown: string) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.description(markdown);
   return callRuntimeMethod("description", markdown);
 };
 
 export const descriptionHtml = (html: string) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.descriptionHtml(html);
   return callRuntimeMethod("descriptionHtml", html);
 };
 
 export const displayName = (name: string) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.displayName(name);
   return callRuntimeMethod("displayName", name);
 };
 
 export const historyId = (value: string) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.historyId(value);
   return callRuntimeMethod("historyId", value);
 };
 
 export const testCaseId = (value: string) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.testCaseId(value);
   return callRuntimeMethod("testCaseId", value);
 };
 
 export const attachment = (name: string, content: Buffer | string, type: ContentType | string) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.attachment(name, content, type);
   return callRuntimeMethod("attachment", name, content, type);
 };
 
@@ -106,20 +74,14 @@ export type StepContext = {
 
 const stepContext: () => StepContext = () => ({
   displayName: (name: string) => {
-    // return runtime.stepDisplayName(name);
     return callRuntimeMethod("stepDisplayName", name);
   },
   parameter: (name, value, mode?) => {
-    // return runtime.stepParameter(name, value, mode);
     return callRuntimeMethod("stepParameter", name, value, mode);
   },
 });
 
 export const step = <T = void>(name: string, body: (context: StepContext) => T | PromiseLike<T>) => {
-  // throw new Error("Not implemented");
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.step(name, () => body(stepContext(runtime)));
   return callRuntimeMethod("step", name, () => body(stepContext()));
 };
 
@@ -150,8 +112,5 @@ export const layer = (name: string) => label(LabelName.LAYER, name);
 export const tag = (name: string) => label(LabelName.TAG, name);
 
 export const tags = (...tagsList: string[]) => {
-  // const runtime = await getGlobalTestRuntimeWithAutoconfig();
-  //
-  // return runtime.labels(...tagsList.map((value) => ({ name: LabelName.TAG, value })));
   return callRuntimeMethod("labels", ...tagsList.map((value) => ({ name: LabelName.TAG, value })));
 };
