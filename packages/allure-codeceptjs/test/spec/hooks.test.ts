d9aab9408b8aa5f63cd7b2c80772140c904ded48
epszaw <konstantin@epishev.me>
2024-06-05 20:07:58 +0200

    implement hooks handlers for codeceptjs

--- packages/allure-codeceptjs/test/spec/hooks.test.ts
+++ packages/allure-codeceptjs/test/spec/hooks.test.ts
@@ -3,11 +3,20 @@ import { Stage, Status } from "allure-js-commons";
 import { runCodeceptJsInlineTest } from "../utils.js";
 
 it("handles hooks", async () => {
-  const { tests } = await runCodeceptJsInlineTest({
+  const { tests, groups } = await runCodeceptJsInlineTest({
     "sample.test.js": `
-      Before(() => {});
-
       Feature("sample-feature");
+
+      BeforeAll(() => {});
+      AfterAll(() => {});
+
+      Before(() => {});
+      After(() => {});
+
+      // these hooks shouldn't be reported because codeceptjs doesn't provide any information about them
+      BeforeSuite(() => {});
+      AfterSuite(() => {});
+
       Scenario("sample-scenario", async ({ I }) => {
         I.pass();
       });
@@ -15,18 +24,49 @@ it("handles hooks", async () => {
   });
 
   expect(tests).toHaveLength(1);
-  expect(tests[0].steps).toHaveLength(2);
-  expect(tests[0].steps).toEqual(
+  expect(tests[0].steps).toHaveLength(1);
+  expect(groups).toHaveLength(4);
+  expect(groups).toEqual(
     expect.arrayContaining([
       expect.objectContaining({
-        name: "before hook",
-        status: Status.PASSED,
-        stage: Stage.FINISHED,
+        name: "\"before all\" hook: BeforeSuite for \"sample-scenario\"",
+        befores: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            stage: Stage.FINISHED,
+          }),
+        ]),
+        afters: [],
       }),
       expect.objectContaining({
-        name: "I pass",
-        status: Status.PASSED,
-        stage: Stage.FINISHED,
+        name: "\"before each\" hook: Before for \"sample-scenario\"",
+        befores: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            stage: Stage.FINISHED,
+          }),
+        ]),
+        afters: [],
+      }),
+      expect.objectContaining({
+        name: "\"after each\" hook: After for \"sample-scenario\"",
+        befores: [],
+        afters: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            stage: Stage.FINISHED,
+          }),
+        ]),
+      }),
+      expect.objectContaining({
+        name: "\"after all\" hook: AfterSuite for \"sample-scenario\"",
+        befores: [],
+        afters: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            stage: Stage.FINISHED,
+          }),
+        ]),
       }),
     ]),
   );
