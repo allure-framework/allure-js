84b31ff14dc60e52042a3c316d351651e2ff912e
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2024-06-11 20:43:02 +0700

    Use commons for mocha's host/thread labels

--- packages/allure-mocha/src/utils.ts
+++ packages/allure-mocha/src/utils.ts
@@ -1,11 +1,10 @@
 import type * as Mocha from "mocha";
-import { hostname } from "node:os";
 import { dirname, extname, join } from "node:path";
-import { env, pid } from "node:process";
+import { env } from "node:process";
 import { fileURLToPath } from "node:url";
-import { isMainThread, threadId } from "node:worker_threads";
 import type { Label } from "allure-js-commons";
 import { LabelName } from "allure-js-commons";
+import { getHostLabel, getThreadLabel } from "allure-js-commons/sdk/reporter";
 
 const filename = fileURLToPath(import.meta.url);
 
@@ -14,23 +13,9 @@ export const getSuitesOfMochaTest = (test: Mocha.Test) => test.titlePath().slice
 export const resolveParallelModeSetupFile = () =>
   join(dirname(filename), `setupAllureMochaParallel${extname(filename)}`);
 
-export const resolveMochaWorkerId = () => env.MOCHA_WORKER_ID ?? (isMainThread ? pid : threadId).toString();
-
-const allureHostName = env.ALLURE_HOST_NAME || hostname();
-
-export const getHostLabel = (): Label => ({
-  name: LabelName.HOST,
-  value: allureHostName,
-});
-
-export const getWorkerIdLabel = (): Label => ({
-  name: LabelName.THREAD,
-  value: resolveMochaWorkerId(),
-});
-
 export const getInitialLabels = (): Label[] => [
   { name: LabelName.LANGUAGE, value: "javascript" },
   { name: LabelName.FRAMEWORK, value: "mocha" },
   getHostLabel(),
-  getWorkerIdLabel(),
+  getThreadLabel(env.MOCHA_WORKER_ID),
 ];
