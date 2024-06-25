6f046d50ba3f6293cc27074c6adbcc8df3adce57
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2024-06-25 16:23:59 +0700

    Fix data loss for pending tests
    Related to #457

---
+++ packages/allure-mocha/test/spec/defects/dynamicSkip.spec.ts
@@ -0,0 +1,14 @@
+import { expect, it } from "vitest";
+import { issue } from "allure-js-commons";
+import { runMochaInlineTest } from "../../utils.js";
+
+it("shouldn't overwrite the result of a dynamically skipped test", async () => {
+  const { tests } = await runMochaInlineTest(["labels", "skippedAtRuntime"], ["legacy", "labels", "skippedAtRuntime"]);
+
+  expect(tests).toHaveLength(2);
+  for (const test of tests) {
+    expect(test).toMatchObject({
+      labels: expect.arrayContaining([expect.objectContaining({ name: "tag", value: "foo" })]),
+    });
+  }
+});
