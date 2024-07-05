422b9b28505f3b34e6cf01304d41bf1aaa28ddb2
Dmitry Baev <baev.dm@gmail.com>
2024-07-05 17:20:29 +0100

    tests(allure-jest): add tests to showcase runtime api usage in hooks (fixes #946, via #1050)

--- packages/allure-jest/test/spec/hooks.test.ts
+++ packages/allure-jest/test/spec/hooks.test.ts
@@ -143,3 +143,271 @@ it("reports failed hooks", async () => {
     ]),
   );
 });
+
+it("should support steps in beforeAll hooks", async () => {
+  const { tests, groups } = await runJestInlineTest({
+    "sample.test.js": `
+      const { logStep } = require("allure-js-commons");
+
+      beforeAll(async () => {
+        await logStep("step 1");
+        await logStep("step 2");
+      });
+
+      it("passed 1", () => {});
+
+      it("passed 2", () => {});
+    `,
+  });
+
+  expect(tests).toHaveLength(2);
+  expect(tests).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+        name: "passed 1",
+      }),
+      expect.objectContaining({
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+        name: "passed 2",
+      }),
+    ]),
+  );
+
+  const [{ uuid: test1Uuid }, { uuid: test2Uuid }] = tests;
+
+  expect(groups).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        name: "beforeAll",
+        children: expect.arrayContaining([test1Uuid, test2Uuid]),
+        befores: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            name: "beforeAll",
+            steps: [
+              expect.objectContaining({
+                name: "step 1",
+              }),
+              expect.objectContaining({
+                name: "step 2",
+              }),
+            ],
+          }),
+        ]),
+      }),
+    ]),
+  );
+});
+
+it("should support steps in afterAll hooks", async () => {
+  const { tests, groups } = await runJestInlineTest({
+    "sample.test.js": `
+      const { logStep } = require("allure-js-commons");
+
+      afterAll(async () => {
+        await logStep("step 1");
+        await logStep("step 2");
+      });
+
+      it("passed 1", () => {});
+
+      it("passed 2", () => {});
+    `,
+  });
+
+  expect(tests).toHaveLength(2);
+  expect(tests).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+        name: "passed 1",
+      }),
+      expect.objectContaining({
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+        name: "passed 2",
+      }),
+    ]),
+  );
+
+  const [{ uuid: test1Uuid }, { uuid: test2Uuid }] = tests;
+
+  expect(groups).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        name: "afterAll",
+        children: expect.arrayContaining([test1Uuid, test2Uuid]),
+        afters: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            name: "afterAll",
+            steps: [
+              expect.objectContaining({
+                name: "step 1",
+              }),
+              expect.objectContaining({
+                name: "step 2",
+              }),
+            ],
+          }),
+        ]),
+      }),
+    ]),
+  );
+});
+
+it("should support steps in beforeEach hooks", async () => {
+  const { tests, groups } = await runJestInlineTest({
+    "sample.test.js": `
+      const { logStep } = require("allure-js-commons");
+
+      beforeEach(async () => {
+        await logStep("step 1");
+        await logStep("step 2");
+      });
+
+      it("passed 1", () => {});
+
+      it("passed 2", () => {});
+    `,
+  });
+
+  expect(tests).toHaveLength(2);
+  expect(tests).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+        name: "passed 1",
+      }),
+      expect.objectContaining({
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+        name: "passed 2",
+      }),
+    ]),
+  );
+
+  const [{ uuid: test1Uuid }, { uuid: test2Uuid }] = tests;
+
+  expect(groups).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        name: "beforeEach",
+        children: expect.arrayContaining([test1Uuid]),
+        befores: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            name: "beforeEach",
+            steps: [
+              expect.objectContaining({
+                name: "step 1",
+              }),
+              expect.objectContaining({
+                name: "step 2",
+              }),
+            ],
+          }),
+        ]),
+      }),
+      expect.objectContaining({
+        name: "beforeEach",
+        children: expect.arrayContaining([test2Uuid]),
+        befores: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            name: "beforeEach",
+            steps: [
+              expect.objectContaining({
+                name: "step 1",
+              }),
+              expect.objectContaining({
+                name: "step 2",
+              }),
+            ],
+          }),
+        ]),
+      }),
+    ]),
+  );
+});
+
+it("should support steps in afterEach hooks", async () => {
+  const { tests, groups } = await runJestInlineTest({
+    "sample.test.js": `
+      const { logStep } = require("allure-js-commons");
+
+      afterEach(async () => {
+        await logStep("step 1");
+        await logStep("step 2");
+      });
+
+      it("passed 1", () => {});
+
+      it("passed 2", () => {});
+    `,
+  });
+
+  expect(tests).toHaveLength(2);
+  expect(tests).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+        name: "passed 1",
+      }),
+      expect.objectContaining({
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+        name: "passed 2",
+      }),
+    ]),
+  );
+
+  const [{ uuid: test1Uuid }, { uuid: test2Uuid }] = tests;
+
+  expect(groups).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        name: "afterEach",
+        children: expect.arrayContaining([test1Uuid]),
+        afters: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            name: "afterEach",
+            steps: [
+              expect.objectContaining({
+                name: "step 1",
+              }),
+              expect.objectContaining({
+                name: "step 2",
+              }),
+            ],
+          }),
+        ]),
+      }),
+      expect.objectContaining({
+        name: "afterEach",
+        children: expect.arrayContaining([test2Uuid]),
+        afters: expect.arrayContaining([
+          expect.objectContaining({
+            status: Status.PASSED,
+            name: "afterEach",
+            steps: [
+              expect.objectContaining({
+                name: "step 1",
+              }),
+              expect.objectContaining({
+                name: "step 2",
+              }),
+            ],
+          }),
+        ]),
+      }),
+    ]),
+  );
+});
