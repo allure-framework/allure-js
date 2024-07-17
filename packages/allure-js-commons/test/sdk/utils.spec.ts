fabfad06bfe1fc40cb6a1972060b76d9789a20b1
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2024-07-17 17:09:21 +0700

    fix(commons): potentially polynomial meta regexps
    Also add tests for extractMetadataFromString.

--- packages/allure-js-commons/test/sdk/utils.spec.ts
+++ packages/allure-js-commons/test/sdk/utils.spec.ts
@@ -2,7 +2,12 @@ import assert from "node:assert";
 import { describe, expect, it } from "vitest";
 import { LabelName, Status } from "../../src/model.js";
 import type { FixtureResult, StepResult, TestResult } from "../../src/model.js";
-import { allureLabelRegexp, getStatusFromError, isAnyStepFailed } from "../../src/sdk/utils.js";
+import {
+  allureLabelRegexp,
+  extractMetadataFromString,
+  getStatusFromError,
+  isAnyStepFailed,
+} from "../../src/sdk/utils.js";
 
 type Executable = StepResult | TestResult | FixtureResult;
 
@@ -171,6 +176,60 @@ describe("getStatusFromError", () => {
       } catch (err) {
         expect(getStatusFromError(err as Error)).toBe(Status.BROKEN);
       }
+    });
+  });
+});
+
+describe("extractMetadataFromString", () => {
+  it("should preserve the title if no matches", () => {
+    expect(extractMetadataFromString("")).toEqual({
+      cleanTitle: "",
+      labels: [],
+    });
+    expect(extractMetadataFromString("foo")).toEqual({
+      cleanTitle: "foo",
+      labels: [],
+    });
+  });
+
+  it("should extract Allure ID", () => {
+    expect(extractMetadataFromString("foo @allure.id:1004")).toEqual({
+      cleanTitle: "foo",
+      labels: [{ name: "ALLURE_ID", value: "1004" }],
+    });
+    expect(extractMetadataFromString("foo allure.id:1004")).toEqual({
+      cleanTitle: "foo",
+      labels: [{ name: "ALLURE_ID", value: "1004" }],
+    });
+    expect(extractMetadataFromString("foo @allure.id=1004")).toEqual({
+      cleanTitle: "foo",
+      labels: [{ name: "ALLURE_ID", value: "1004" }],
+    });
+  });
+
+  it("should extract label", () => {
+    expect(extractMetadataFromString("foo @allure.label.bar:baz")).toEqual({
+      cleanTitle: "foo",
+      labels: [{ name: "bar", value: "baz" }],
+    });
+    expect(extractMetadataFromString("foo allure.label.bar:baz")).toEqual({
+      cleanTitle: "foo",
+      labels: [{ name: "bar", value: "baz" }],
+    });
+    expect(extractMetadataFromString("foo @allure.label.bar=baz")).toEqual({
+      cleanTitle: "foo",
+      labels: [{ name: "bar", value: "baz" }],
+    });
+  });
+
+  it("should extract all matches", () => {
+    expect(extractMetadataFromString("foo @allure.label.bar:baz @allure.id:1004 @allure.label.qux:quz")).toEqual({
+      cleanTitle: "foo",
+      labels: [
+        { name: "bar", value: "baz" },
+        { name: "ALLURE_ID", value: "1004" },
+        { name: "qux", value: "quz" },
+      ],
     });
   });
 });
