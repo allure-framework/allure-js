767bfabaf500a9ab6612278f230e7997785d1f51
Dmitry Baev <baev@users.noreply.github.com>
2024-08-09 17:44:42 +0100

    add support for screenshotOnFail plugin

--- packages/allure-codeceptjs/src/index.ts
+++ packages/allure-codeceptjs/src/index.ts
@@ -1,4 +1,5 @@
 import { container } from "codeceptjs";
+import { attachment } from "allure-js-commons";
 import type { ReporterConfig } from "allure-js-commons/sdk/reporter";
 import { allureCodeceptJsLegacyApi } from "./legacy.js";
 import { AllureCodeceptJsReporter } from "./reporter.js";
@@ -7,7 +8,14 @@ const allurePlugin = (config: ReporterConfig) => {
   const mocha = container.mocha();
   mocha.reporter(AllureCodeceptJsReporter.prototype.constructor, { ...config });
 
-  return allureCodeceptJsLegacyApi;
+  return {
+    ...allureCodeceptJsLegacyApi,
+    // this method is used by various bundled codeceptjs plugins, e.g. by screenshotOnFail
+    addAttachment: (name: string, content: Buffer | string, contentType: string) => {
+      // wrap it in attachmentStep. Since we use Mocha, Runtime API is sync, so no awaits is fine
+      attachment(name, content, contentType);
+    },
+  };
 };
 
 export default allurePlugin;
