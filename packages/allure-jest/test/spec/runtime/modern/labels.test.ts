4b3ddb9c6b46454fe43afb47bb076be8ed6788a5
Dmitry Baev <baev.dm@gmail.com>
2024-09-20 15:08:38 +0100

    fix(allure-js-commons): fix hooks metadata processing (fixes #1144, via #1145)

--- packages/allure-jest/test/spec/runtime/modern/labels.test.ts
+++ packages/allure-jest/test/spec/runtime/modern/labels.test.ts
@@ -55,3 +55,59 @@ it("sets labels", async () => {
   expect(tests[0].labels).toContainEqual({ name: "test", value: "testValue" });
   expect(tests[0].labels).toContainEqual({ name: "test2", value: "testValue2" });
 });
+
+it("should set labels from beforeEach", async () => {
+  const { tests } = await runJestInlineTest({
+    "sample.test.js": `
+      const {
+        label,
+        labels,
+        feature,
+        allureId,
+        epic,
+        layer,
+        owner,
+        parentSuite,
+        suite,
+        subSuite,
+        severity,
+        story,
+        tag,
+      } = require("allure-js-commons");
+
+      beforeEach(async () => {
+        await label("foo", "bar");
+        await allureId("foo");
+        await epic("foo");
+        await feature("foo");
+        await layer("foo");
+        await owner("foo");
+        await parentSuite("foo");
+        await subSuite("foo");
+        await suite("foo");
+        await severity("foo");
+        await story("foo");
+        await tag("foo");
+        await labels({ name: "test", value: "testValue" }, { name: "test2", value: "testValue2" });
+      });
+
+       it("label", () => {});
+    `,
+  });
+
+  expect(tests).toHaveLength(1);
+  expect(tests[0].labels).toContainEqual({ name: "foo", value: "bar" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.ALLURE_ID, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.EPIC, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.FEATURE, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.LAYER, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.OWNER, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.PARENT_SUITE, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.SUB_SUITE, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.SUITE, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.SEVERITY, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.STORY, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: LabelName.TAG, value: "foo" });
+  expect(tests[0].labels).toContainEqual({ name: "test", value: "testValue" });
+  expect(tests[0].labels).toContainEqual({ name: "test2", value: "testValue2" });
+});
