a566e1e70e56af4d9e79cdf3909d86f0547892dd
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2024-10-02 03:22:00 +0700

    refactor(commons): parseEnvInfo implementation

--- packages/allure-js-commons/test/sdk/reporter/utils/envInfo.spec.ts
+++ packages/allure-js-commons/test/sdk/reporter/utils/envInfo.spec.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import properties from "properties";
 import { describe, expect, it } from "vitest";
-import { stringifyEnvInfo } from "../../../../src/sdk/reporter/utils/envInfo.js";
+import { parseEnvInfo, stringifyEnvInfo } from "../../../../src/sdk/reporter/utils/envInfo.js";
 import type { EnvironmentInfo } from "../../../../src/sdk/types.js";
 
 describe("stringifyEnvInfo", () => {
@@ -19,6 +19,29 @@ describe("stringifyEnvInfo", () => {
 
   it("should escape whitespaces in a key", () => {
     expect(stringifyEnvInfo({ " foo bar ": "baz" })).toEqual("\\ foo\\ bar\\ =baz");
+  });
+
+  it("should escape colon in a key", () => {
+    expect(stringifyEnvInfo({ "foo:bar": "baz" })).toEqual("foo\\:bar=baz");
+  });
+
+  it("should escape equal sign in a key", () => {
+    expect(stringifyEnvInfo({ "foo=bar": "baz" })).toEqual("foo\\=bar=baz");
+  });
+
+  it("should escape control chars in a key", () => {
+    expect(stringifyEnvInfo({ "foo\\bar": "baz" })).toEqual("foo\\\\bar=baz");
+    expect(stringifyEnvInfo({ "foo\tbar": "baz" })).toEqual("foo\\tbar=baz");
+    expect(stringifyEnvInfo({ "foo\nbar": "baz" })).toEqual("foo\\nbar=baz");
+    expect(stringifyEnvInfo({ "foo\fbar": "baz" })).toEqual("foo\\fbar=baz");
+    expect(stringifyEnvInfo({ "foo\rbar": "baz" })).toEqual("foo\\rbar=baz");
+  });
+
+  it("should escape chars from control sets 0 and 1 in a key", () => {
+    expect(stringifyEnvInfo({ "foo\0bar": "baz" })).toEqual("foo\\u0000bar=baz");
+    expect(stringifyEnvInfo({ "foo\x1Fbar": "baz" })).toEqual("foo\\u001fbar=baz");
+    expect(stringifyEnvInfo({ "foo\x7Fbar": "baz" })).toEqual("foo\\u007fbar=baz");
+    expect(stringifyEnvInfo({ "foo\x80bar": "baz" })).toEqual("foo\\u0080bar=baz");
   });
 
   it("should escape whitespaces at the beginning of a value", () => {
@@ -47,19 +70,86 @@ describe("stringifyEnvInfo", () => {
     expect(stringifyEnvInfo({ foo: "\x80" })).toEqual("foo=\\u0080");
   });
 
-  it("should escape unicode", () => {
-    expect(stringifyEnvInfo({ foo: "Ψ" })).toEqual("foo=\\u03a8");
-    expect(stringifyEnvInfo({ foo: "𝆔" })).toEqual("foo=\\ud834\\udd94");
+  it("should escape unicode in keys and values", () => {
+    expect(stringifyEnvInfo({ Δ: "Σ" })).toEqual("\\u0394=\\u03a3");
+    expect(stringifyEnvInfo({ "𝅘𝅥𝅲": "𝆔" })).toEqual("\\ud834\\udd64=\\ud834\\udd94");
+  });
+
+  it("should skip null and undefined values", () => {
+    expect(stringifyEnvInfo({ foo: undefined })).toEqual("");
+    expect(stringifyEnvInfo({ foo: null } as any as EnvironmentInfo)).toEqual("");
   });
 
   it("should be compatible with properties.parse", () => {
     const envInfo: EnvironmentInfo = {
       foo: "bar",
-      [" : = "]: "baz",
+      [" : = \t\f\\\n\r\0\x7f\x80"]: "baz",
       qux: " \t\f \\\t\n\f\r \0\x7f\x80",
       quux: "𝆔Ψ𝆔",
+      ["𝅘𝅥𝅲"]: "𝆔",
     };
 
     expect(properties.parse(stringifyEnvInfo(envInfo), { unicode: true })).toEqual(envInfo);
+  });
+});
+
+describe("parseEnvInfo", () => {
+  it("should parse empty string", () => {
+    expect(parseEnvInfo("")).toEqual({});
+  });
+
+  it("should parse single kv-pair", () => {
+    expect(parseEnvInfo("foo=bar")).toEqual({ foo: "bar" });
+  });
+
+  it("should parse multiple kv-pairs", () => {
+    expect(parseEnvInfo("foo=bar\nbaz=qux")).toEqual({ foo: "bar", baz: "qux" });
+  });
+
+  it("should ignore extra whitespaces", () => {
+    expect(parseEnvInfo(" \t\f\rfoo \t\f\r= \t\f\rbar\r\n")).toEqual({ foo: "bar" });
+  });
+
+  it("should support multiline values", () => {
+    expect(parseEnvInfo("foo=bar\\\n    baz")).toEqual({ foo: "barbaz" });
+  });
+
+  it("should decode keys", () => {
+    expect(parseEnvInfo("\\==foo")).toEqual({ "=": "foo" });
+    expect(parseEnvInfo("\\:=foo")).toEqual({ ":": "foo" });
+    expect(parseEnvInfo("\\ =foo")).toEqual({ " ": "foo" });
+    expect(parseEnvInfo("\\t=foo")).toEqual({ "\t": "foo" });
+    expect(parseEnvInfo("\\n=foo")).toEqual({ "\n": "foo" });
+    expect(parseEnvInfo("\\f=foo")).toEqual({ "\f": "foo" });
+    expect(parseEnvInfo("\\r=foo")).toEqual({ "\r": "foo" });
+    expect(parseEnvInfo("\\u007f=foo")).toEqual({ "\x7f": "foo" });
+    expect(parseEnvInfo("\\u0080=foo")).toEqual({ "\x80": "foo" });
+  });
+
+  it("should decode values", () => {
+    expect(parseEnvInfo("foo=\\ ")).toEqual({ foo: " " });
+    expect(parseEnvInfo("foo=\\r")).toEqual({ foo: "\r" });
+    expect(parseEnvInfo("foo=\\f")).toEqual({ foo: "\f" });
+    expect(parseEnvInfo("foo=\\n")).toEqual({ foo: "\n" });
+    expect(parseEnvInfo("foo=\\t")).toEqual({ foo: "\t" });
+    expect(parseEnvInfo("foo=\\u007f")).toEqual({ foo: "\x7f" });
+    expect(parseEnvInfo("foo=\\u0080")).toEqual({ foo: "\x80" });
+  });
+
+  it("should decode unicode", () => {
+    expect(parseEnvInfo("\\u0394=\\u03a3")).toEqual({ Δ: "Σ" });
+    expect(parseEnvInfo("\\ud834\\udd64=\\ud834\\udd94")).toEqual({ "𝅘𝅥𝅲": "𝆔" });
+  });
+
+  it("should be compatible with stringifyEnvInfo", () => {
+    const envInfo: EnvironmentInfo = {
+      foo: "bar",
+      [" : = \t\f\\\n\r\0\x7f\x80"]: "baz",
+      qux: " \t\f \\\t\n\f\r \0\x7f\x80",
+      quux: "𝆔Ψ𝆔",
+      ["𝅘𝅥𝅲"]: "𝆔",
+    };
+
+    expect(parseEnvInfo(stringifyEnvInfo(envInfo))).toEqual(envInfo);
   });
 });
