aa2ce0b894f86f405c9ecb11f380e0776c2f7b81
epszaw <konstantin@epishev.me>
2025-07-08 13:49:45 +0000

    Bump image-size to 3.0.3 (#1309)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- .pnp.cjs
+++ .pnp.cjs
@@ -10608,14 +10608,6 @@ const RAW_RUNTIME_STATE =
           ["fill-range", "npm:7.1.1"]\
         ],\
         "linkType": "HARD"\
-      }],\
-      ["npm:3.0.3", {\
-        "packageLocation": "./.yarn/cache/braces-npm-3.0.3-582c14023c-fad11a0d46.zip/node_modules/braces/",\
-        "packageDependencies": [\
-          ["braces", "npm:3.0.3"],\
-          ["fill-range", "npm:7.1.1"]\
-        ],\
-        "linkType": "HARD"\
       }]\
     ]],\
     ["brotli", [\
@@ -13924,14 +13916,6 @@ const RAW_RUNTIME_STATE =
           ["to-regex-range", "npm:5.0.1"]\
         ],\
         "linkType": "HARD"\
-      }],\
-      ["npm:7.1.1", {\
-        "packageLocation": "./.yarn/cache/fill-range-npm-7.1.1-bf491486db-a7095cb39e.zip/node_modules/fill-range/",\
-        "packageDependencies": [\
-          ["fill-range", "npm:7.1.1"],\
-          ["to-regex-range", "npm:5.0.1"]\
-        ],\
-        "linkType": "HARD"\
       }]\
     ]],\
     ["finalhandler", [\
@@ -15123,10 +15107,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["image-size", [\
-      ["npm:1.2.0", {\
-        "packageLocation": "./.yarn/cache/image-size-npm-1.2.0-3e4c04029c-b4015e7114.zip/node_modules/image-size/",\
+      ["npm:1.2.1", {\
+        "packageLocation": "./.yarn/cache/image-size-npm-1.2.1-e285f3c080-b290c6cc56.zip/node_modules/image-size/",\
         "packageDependencies": [\
-          ["image-size", "npm:1.2.0"],\
+          ["image-size", "npm:1.2.1"],\
           ["queue", "npm:6.0.2"]\
         ],\
         "linkType": "HARD"\
@@ -17593,7 +17577,7 @@ const RAW_RUNTIME_STATE =
           ["flow-enums-runtime", "npm:0.0.6"],\
           ["graceful-fs", "npm:4.2.11"],\
           ["hermes-parser", "npm:0.23.1"],\
-          ["image-size", "npm:1.2.0"],\
+          ["image-size", "npm:1.2.1"],\
           ["invariant", "npm:2.2.4"],\
           ["jest-worker", "npm:29.7.0"],\
           ["jsc-safe-url", "npm:0.2.4"],\
@@ -17642,7 +17626,7 @@ const RAW_RUNTIME_STATE =
           ["flow-enums-runtime", "npm:0.0.6"],\
           ["graceful-fs", "npm:4.2.11"],\
           ["hermes-parser", "npm:0.24.0"],\
-          ["image-size", "npm:1.2.0"],\
+          ["image-size", "npm:1.2.1"],\
           ["invariant", "npm:2.2.4"],\
           ["jest-worker", "npm:29.7.0"],\
           ["jsc-safe-url", "npm:0.2.4"],\
