e0b5da38bc01e6bb2a86c42362962841c7ace6c6
Dmitry Baev <baev@users.noreply.github.com>
2026-05-12 17:33:35 +0100

    enable tests on ci

--- scripts/ci/run-suite.mjs
+++ scripts/ci/run-suite.mjs
@@ -59,6 +59,7 @@ const suites = new Map([
       compileFrom: [
         "allure-js-commons",
         "allure-bun",
+        "allure-chai",
         "allure-jasmine",
         "allure-jest",
         "allure-cucumberjs",
@@ -68,6 +69,7 @@ const suites = new Map([
       testCommands: [
         createWorkspaceScriptCommand("allure-js-commons", "test"),
         createWorkspaceScriptCommand("allure-bun", "test"),
+        createWorkspaceScriptCommand("allure-chai", "test"),
         createWorkspaceScriptCommand("allure-cucumberjs", "test"),
         createWorkspaceScriptCommand("allure-jasmine", "test"),
         createWorkspaceScriptCommand("allure-jest", "test"),
